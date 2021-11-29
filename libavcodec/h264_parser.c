a0aa4aa8c5c97486b939a0c2141072e384b07a7b
akarin <i@akarin.info>
2021-11-29 18:45:48 +0900

    libavcodec/h264_parser.c: set key_frame to 2 if recovery point SEI is found
    This should help AkarinVS/L-SMASH-Works#19.
    
    Signed-off-by: akarin <i@akarin.info>

--- libavcodec/h264_parser.c
+++ libavcodec/h264_parser.c
@@ -348,7 +348,7 @@ static inline int parse_nal_units(AVCodecParserContext *s,
             s->pict_type = ff_h264_golomb_to_pict_type[slice_type % 5];
             if (p->sei.recovery_point.recovery_frame_cnt >= 0) {
                 /* key frame, since recovery_frame_cnt is set */
-                s->key_frame = 1;
+                s->key_frame = 2;
             }
             pps_id = get_ue_golomb(&nal.gb);
             if (pps_id >= MAX_PPS_COUNT) {
